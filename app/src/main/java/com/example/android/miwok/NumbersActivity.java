c33529c91ce1d966c6948c90f8a913a7866701d2
joe-BlanketTheThird <popeofpoetry@yahoo.com>
2016-06-30 12:47:01 -0600

    Ditched that slow, old for loop and put in a fast, new ArrayAdapter to recycle views.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -42,12 +44,10 @@ public class NumbersActivity extends AppCompatActivity {
 	words.add("Nine");
 	words.add("Ten");
 
-	LinearLayout rootView = (LinearLayout)findViewById(R.id.rootview);
 
-	for(int index = 0; index < words.size(); index++){
-		TextView wordView = new TextView(this);
-		wordView.setText(words.get(index));
-		rootView.addView(wordView);
-		}
+		ArrayAdapter<String> itemsAdapter =
+				new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+		ListView listView = (ListView) findViewById(R.id.list);
+		listView.setAdapter(itemsAdapter);
 	}
 }
