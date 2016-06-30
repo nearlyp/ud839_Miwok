ad285a56ec311aa5a56b12e8cb49cfb66ff8d4dd
joe-pixel-c <popeofpoetry@yahoo.com>
2016-06-29 23:35:03 -0600

    Switched array to ArrayList.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -26,18 +26,18 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 	
-	String[] words = new String[10];
-	words[0] = "one";
-	words[1] = "two";
-	words[2] = "three";
-	words[3] = "four";
-	words[4] = "five";
-	words[5] = "six";
-	words[6] = "seven";
-	words[7] = "eight";
-	words[8] = "nine";
-	words[9] = "ten";
+	ArrayList<String> words = new ArrayList<String>;
+	words.add("one";
+	words.add("two";
+	words.add("three";
+	words.add("four";
+	words.add("five";
+	words.add("six";
+	words.add("seven";
+	words.add("eight";
+	words.add("nine";
+	words.add("ten";
 
-	Log.v("numbers_activity", "Word at index 5: " + words[5]);
+	Log.v("numbers_activity", "Word at index 5: " + words(5));
 	}
 }
