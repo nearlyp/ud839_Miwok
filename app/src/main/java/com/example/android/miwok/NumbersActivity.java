25b9c3c3b43efe5291c0a16e04f4eff75feab312
joe-BlanketTheThird <popeofpoetry@yahoo.com>
2016-06-30 13:08:27 -0600

    Ditched that slow, old for loop and put in a fast, new ArrayAdapter to recycle views. Experimenting with GridView and about to attempt to get Instant Run supported.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -19,6 +19,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -46,8 +47,8 @@ public class NumbersActivity extends AppCompatActivity {
 
 
 		ArrayAdapter<String> itemsAdapter =
-				new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
-		ListView listView = (ListView) findViewById(R.id.list);
+				new ArrayAdapter<>(this, android.R.layout.simple_list_item_checked, words);
+		GridView listView = (GridView) findViewById(R.id.list);
 		listView.setAdapter(itemsAdapter);
 	}
 }
