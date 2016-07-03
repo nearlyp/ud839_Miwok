2cb0338b015238f7f180df46462ced0a50679ce8
joe-BlanketTheThird <popeofpoetry@yahoo.com>
2016-07-02 22:52:23 -0600

    Added audio files to play when individual items are selected.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,34 +15,41 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ColorsActivity extends AppCompatActivity {
 
+    private MediaPlayer mMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<>();
+        final ArrayList<Word> words = new ArrayList<>();
         words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
-        words.add(new Word("green", "chokokki", R.drawable.color_green));
-        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown));
-        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray));
-        words.add(new Word("black", "kululli", R.drawable.color_black));
-        words.add(new Word("white", "kelelli", R.drawable.color_white));
-        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow));
-        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow));
+        words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
 
         WordAdapter adapter =
@@ -51,5 +58,19 @@ public class ColorsActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+
+                Word word = words.get(i);
+
+                mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getSoundFileID());
+
+                mMediaPlayer.start();
+
+                Toast.makeText(ColorsActivity.this, "Playing sound", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -15,36 +15,42 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FamilyActivity extends AppCompatActivity {
 
+    private MediaPlayer mMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("mother", "әpә", R.drawable.family_mother));
-        words.add(new Word("father", "әṭa",R.drawable.family_father));
-        words.add(new Word("daughter", "angsi", R.drawable.family_daughter));
-        words.add(new Word("son", "tune", R.drawable.family_son));
-        words.add(new Word("older sister", "taachi", R.drawable.family_older_sister));
-        words.add(new Word("younger sister", "chalitti", R.drawable.family_younger_sister));
-        words.add(new Word("older brother", "teṭe", R.drawable.family_older_brother));
-        words.add(new Word("younger brother", "kolliti", R.drawable.family_younger_brother));
-        words.add(new Word("grandfather ", "ama", R.drawable.family_grandfather));
-        words.add(new Word("grandmother", "paapa", R.drawable.family_grandmother));
+        final ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("mother", "әpә", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("father", "әṭa",R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("daughter", "angsi", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("son", "tune", R.drawable.family_son, R.raw.family_son));
+        words.add(new Word("older sister", "taachi", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new Word("younger sister", "chalitti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        words.add(new Word("older brother", "teṭe", R.drawable.family_older_brother, R.raw.family_older_brother));
+        words.add(new Word("younger brother", "kolliti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        words.add(new Word("grandfather ", "ama", R.drawable.family_grandfather, R.raw.family_grandfather));
+        words.add(new Word("grandmother", "paapa", R.drawable.family_grandmother, R.raw.family_grandmother));
 
 
         WordAdapter adapter =
@@ -53,5 +59,19 @@ public class FamilyActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+
+                Word word = words.get(i);
+
+                mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getSoundFileID());
+
+                mMediaPlayer.start();
+
+                Toast.makeText(FamilyActivity.this, "Playing sound", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
